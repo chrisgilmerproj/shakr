d2ed56deabfc1e58e3478846ec1be494c3e43e7a
Chris Gilmer <chris.gilmer@gmail.com>
2011-04-24 07:49:40 -0700

    Added all USGS Earthquake Feeds

--- shakr.py
+++ shakr.py
@@ -2,7 +2,16 @@
 
 import binascii, feedparser, glob, pprint, serial, struct, sys, time
 
-USGS = 'http://earthquake.usgs.gov/earthquakes/catalogs/eqs1hour-M0.xml'
+# USGS Earthquake Feeds
+USGS_1h_M1  = 'http://earthquake.usgs.gov/earthquakes/catalogs/eqs1hour-M1.xml'
+USGS_1h_M0  = 'http://earthquake.usgs.gov/earthquakes/catalogs/eqs1hour-M0.xml'
+USGS_1d_M25 = 'http://earthquake.usgs.gov/earthquakes/catalogs/eqs1day-M2.5.xml'
+USGS_1d_M1  = 'http://earthquake.usgs.gov/earthquakes/catalogs/eqs1day-M1.xml'
+USGS_1d_M0  = 'http://earthquake.usgs.gov/earthquakes/catalogs/eqs1day-M0.xml'
+USGS_7d_M7  = 'http://earthquake.usgs.gov/earthquakes/catalogs/eqs7day-M7.xml'
+USGS_7d_M5  = 'http://earthquake.usgs.gov/earthquakes/catalogs/eqs7day-M5.xml'
+USGS_7d_M25 = 'http://earthquake.usgs.gov/earthquakes/catalogs/eqs7day-M2.5.xml'
+USGS_30d    = 'http://earthquake.usgs.gov/earthquakes/shakemap/rss.xml'
 
 def scanports():
 	return glob.glob('/dev/tty*')
@@ -32,7 +41,7 @@ if __name__ == '__main__':
 
 	while 1:
 		# Parse the USGS Feed
-		feed = feedparser.parse(USGS)
+		feed = feedparser.parse(USGS_1h_M0)
 		if len(feed.entries):
 			for entry in feed.entries:
 				# Pull out the title and summary
